cc4a8d10b2fd6a11dcbc9bbe452e5a87b2797bb2
Lauren Small <lauren.small@detroitlabs.com>
2018-10-08 14:59:47 -0400

    created a method to get a summary of the states of the dealt and undealt deck

--- Blackjack/Blackjack/Model/Deck.swift
+++ Blackjack/Blackjack/Model/Deck.swift
@@ -13,16 +13,32 @@ class Deck{
     private var undealtCards: [Card]
     private var dealtCards: [Card]
     
+    var summary: String{
+        return self.getSummary()
+    }
+    
     init(){
         var cards: [Card] = []
-        for suit in Card.legitimateSuits(){ //had to change func legitimateSuits to class func legitimateSuits
-            for rank in Card.legitimateRanks(){ //had to change func legitimateRanks to class func legitimateRanks
+        for suit in Card.legitimateSuits(){ //In Card class, I had to change func legitimateSuits to class func legitimateSuits
+            for rank in Card.legitimateRanks(){ //In Card class, I had to change func legitimateRanks to class func legitimateRanks
                 let card = Card(withSuit: suit, withRank: rank)
                 cards.append(card)
             }
         }
         undealtCards = cards
         dealtCards = []
+    }
+    
+    
+    //getSummary() or getDescription()
+    private func getSummary()-> String{
+        var summary = "Deck"
+        summary += "\n Cards Left: "
+        summary += descriptionFor(cardArray: undealtCards)
+        summary += "\n Cards Dealt: "
+        summary += descriptionFor(cardArray: dealtCards)
+        
+        return summary
     }
     
 }
