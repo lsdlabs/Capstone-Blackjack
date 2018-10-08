de48f8c275fd60f3274d4763662ed94c6990715f
Lauren Small <lauren.small@detroitlabs.com>
2018-10-08 11:05:13 -0400

    Created MVC Folder Structure

---
+++ Blackjack/Blackjack/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blackjack
+//
+//  Created by Lauren Small on 10/8/18.
+//  Copyright © 2018 Lauren Small. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
