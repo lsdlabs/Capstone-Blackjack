dcf798db831af5e6c66a0465dc0dbf9071c4c773
Lauren Small <lauren.small@detroitlabs.com>
2018-10-17 10:27:30 -0400

    Deleted all the code in viewDidLoad

--- Blackjack/Blackjack/Controller/BlackjackViewController.swift
+++ Blackjack/Blackjack/Controller/BlackjackViewController.swift
@@ -27,75 +27,7 @@ class BlackjackViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(deck.summary)//print the deck...make sure all the cards are there
-                            //should print the cards remaining (52) and the number of cards dealt (0)
-        //shuffle the deck, and print it to check
-        showLabel()
-        //gameEventLabel.text = "Welcome!"
-//        if !gameEventLabel.isHidden {
-//            gameEventLabel.isHidden = true
-//            Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(showLabel), userInfo: nil, repeats: false)
-//        }
         
-        gameEventLabel.text = "Shuffling the deck..."
-        deck.shuffle()
-        print(deck.summary)
-        
-        let house = House.init(name: "House")
-        
-        house.cards.append(deck.drawCard())
-        house.cards.append(deck.drawCard())
-        print(house.description)
-        
-        dealerCards.text = "\(house.description)"//
-        
-        
-        
-        
-        while house.mustHit{
-            house.cards.append(deck.drawCard())
-            house.cards.append(deck.drawCard())
-            print(house.description)
-        }
-        
-        while house.mustHit{
-            house.cards.append(deck.drawCard())
-            print(house.description)
-        }
-        
-        self.playBlackjack(withBetOf: 20)
-        
-        
-//        func playBlackjack(withBetOf bet: Int){
-//            let goodBet = dealer.placeBet(bet: bet)
-//            if !goodBet{
-//                return
-//            }
-//            print("The bet is \(bet)")
-//        }
-        
-        print("Deal!")
-        dealer.deal()
-        print()
-        print(dealer.house.description)
-        print(dealer.house.description)
-        
-        for index in 2...4{
-            dealer.turn(house: dealer.player)
-            if !dealer.player.busted{
-                dealer.turn(house: dealer.house)
-            }
-        }
-        
-        print("***End of the game hand***")
-        let award = dealer.award()
-        print(award)
-        print(dealer.house.description)
-        print(dealer.player.description)
-        
-        
-        
-        // Do any additional setup after loading the view.
     }
     
     @objc func showLabel() {
